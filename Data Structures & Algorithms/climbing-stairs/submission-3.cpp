bf68e62e0476c52e8da25edeac5f5057a7b7c4d7
Harshit_V <vaghamshiharshit@gmail.com>
2026-07-03 16:31:12 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    //top down approach
+    vector<int> dp = vector<int>(50, -1);
+
+    int climbStairs(int n) {
+
+        if (n == 0 || n == 1) return 1;
+        
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        
+
+        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        
+        return dp[n];
+    }
+};
